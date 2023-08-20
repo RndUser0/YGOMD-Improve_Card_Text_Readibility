526890d0e6ed3f879463dbfe1262738c50aebda7
RndUser0 <29837561+RndUser0@users.noreply.github.com>
2023-08-20 19:03:26 +0200

    Update _CARD_decrypt_and_split.py
    Added file checks for handling the Card_Part file for correct card effects highlighting.

--- _CARD_decrypt_and_split.py
+++ _CARD_decrypt_and_split.py
@@ -4,36 +4,33 @@ akintos: https://gist.github.com/akintos/04e2494c62184d2d4384078b0511673b
 timelic: https://github.com/timelic/master-duel-chinese-translation-switch
 '''
 
-from typing import List
-import json
+#from typing import List
+#import json
 #import os
-import sys
-import zlib
+#import sys
+#import zlib
 from _defs import *
 
 # 1. Check if CARD_* files exist:
 
-CARD_filenames = Check_CARD_files()
-CARD_Indx_filename = CARD_filenames[0]
-CARD_Desc_filename = CARD_filenames[1]
-CARD_Name_filename = CARD_filenames[2]
+CARD_filename_list = Check_files(['CARD_Indx', 'CARD_Name', 'CARD_Desc', 'Card_Pidx', 'Card_Part'])
+CARD_Indx_filename = CARD_filename_list[0]
+CARD_Name_filename = CARD_filename_list[1]
+CARD_Desc_filename = CARD_filename_list[2]
 
 # 2. Get crypto key
 
 m_iCryptoKey = GetCryptoKey(CARD_Indx_filename)
 
-# 3. Decrypt CARD_Desc, Card_Indx + CARD_Name
+# 3. Decrypt card files from section 1
 
 print('Decrypting files...')
 
-for filename in CARD_filenames:
-	if FileCheck(filename) == 1:
-		data = ReadData(filename)
-		data = Decrypt(data, m_iCryptoKey)
-		WriteDecData(filename, data)
-		print('Decrypted file "' + filename + '".')	
-	else:
-		print("Could not decrypt file " + filename + " because it does not appear to exist.")
+for filename in CARD_filename_list:	
+	data = ReadByteData(filename)
+	data = Decrypt(data, m_iCryptoKey)
+	WriteDecByteData(filename, data)
+	print('Decrypted file "' + filename + '".')
 
 # 4. Split CARD_Name + CARD_Desc
 
